dc9dca7891772b91a37719eaa965bb0cf421dcc5
Kaldek <ed.luck@gmail.com>
2023-11-09 14:44:42 +1100

    Create a data collection sketch
    The calibration tables provided in the simple sketch are now three years old and are not adequate for our goals of accuracy.  This data collection sketch allows us to slowly fill the tank or drain the tank and take measurements by pressing a button connected to the Arduino.
    
    The goal is to be able to build a matrix of behaviour of all three rheostats and the low fuel switch, and use this to create a highly accurate table for driving the fuel gauge.

---
+++ Data-Collection.ino
@@ -0,0 +1,102 @@
+const int buttonPin = 2;
+const int LowFuelSwitch = 3;
+const int ledPin = 13;
+
+int buttonState = 0;        
+int lastButtonState = 0;     
+int switchState = 0;
+
+unsigned long startMillis; 
+unsigned long currentMillis;
+const unsigned long period = 10000;
+bool reading = false;
+int UpperRheostatLowest, UpperRheostatHighest, MainRheostatLowest, MainRheostatHighest, LowFuelRheostatLowest, LowFuelRheostatHighest;
+int analog0, analog1, analog2;
+
+long readVcc() {
+  long result;
+  // Read 1.1V reference against AVcc
+  ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Convert
+  while (bit_is_set(ADCSRA, ADSC));
+  result = ADCL;
+  result |= ADCH << 8;
+  result = 1126400L / result; // Calculate Vcc (in mV); 1126400 = 1.1*1024*1000
+  return result;
+}
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(LowFuelSwitch, INPUT);  
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+  switchState = digitalRead(LowFuelSwitch);
+
+  if (buttonState != lastButtonState) {
+    if (buttonState == HIGH) {
+      Serial.println("Button pressed");
+      digitalWrite(ledPin, HIGH);
+      startMillis = millis();
+      reading = true;
+      UpperRheostatLowest = 1023;
+      UpperRheostatHighest = 0;
+      MainRheostatLowest = 1023;
+      MainRheostatHighest = 0;
+      LowFuelRheostatLowest = 1023;
+      LowFuelRheostatHighest = 0;
+    }
+    delay(50);
+  }
+  
+  lastButtonState = buttonState;
+
+  if (reading) {
+    currentMillis = millis();
+
+    if (currentMillis - startMillis <= period) {
+      long vcc=readVcc();
+      int rawA0 = analogRead(A0);
+      int rawA1 = analogRead(A1);
+      int rawA2 = analogRead(A2);
+      analog0 = rawA0 * vcc / 1024;
+      analog1 = rawA1 * vcc / 1024;
+      analog2 = rawA2 * vcc / 1024;
+      
+
+      if (analog0 < UpperRheostatLowest) UpperRheostatLowest = analog0;
+      if (analog0 > UpperRheostatHighest) UpperRheostatHighest = analog0;
+      if (analog1 < MainRheostatLowest) MainRheostatLowest = analog1;
+      if (analog1 > MainRheostatHighest) MainRheostatHighest = analog1;
+      if (analog2 < LowFuelRheostatLowest) LowFuelRheostatLowest = analog2;
+      if (analog2 > LowFuelRheostatHighest) LowFuelRheostatHighest = analog2;
+    } else {
+      Serial.print("Upper Rheostat Lowest: ");
+      Serial.println(UpperRheostatLowest * (5.0 / 1023.0));
+      Serial.print("Upper Rheostat Highest: ");
+      Serial.println(UpperRheostatHighest * (5.0 / 1023.0));
+      Serial.print("Main Rheostat Lowest: ");
+      Serial.println(MainRheostatLowest * (5.0 / 1023.0));
+      Serial.print("Main Rheostat Highest: ");
+      Serial.println(MainRheostatHighest * (5.0 / 1023.0));
+      Serial.print("Low Fuel Rheostat Lowest: ");  
+      Serial.println(LowFuelRheostatLowest * (5.0 / 1023.0));
+      Serial.print("Low Fuel Rheostat Highest: ");  
+      Serial.println(LowFuelRheostatHighest * (5.0 / 1023.0));
+
+      Serial.print("Low Fuel Switch State: ");
+      if (switchState == HIGH) {
+        Serial.println("CLOSED");
+      } else {
+        Serial.println("OPEN");
+      }
+
+      reading = false;
+      digitalWrite(ledPin, LOW);
+    }
+  }
+}
