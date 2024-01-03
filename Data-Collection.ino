49d645bd161179ba7750df3bebfa6401910f11a0
Kaldek <ed.luck@gmail.com>
2024-01-03 18:20:06 +1100

    Major changes to fix fundamental bugs

--- Data-Collection.ino
+++ Data-Collection.ino
@@ -11,15 +11,18 @@ unsigned long currentMillis;
 const unsigned long period = 10000;
 bool reading = false;
 int UpperRheostatLowest, UpperRheostatHighest, MainRheostatLowest, MainRheostatHighest;
-float analog0, analog1;
 
 // Constants for fixed resistors in the voltage divider
 const float UpperRheostatFixedResistor = 33.0; // Ohms
 const float MainRheostatFixedResistor = 22.0;  // Ohms
+const float ReferenceVoltage = 4.96; // Nano Every +5V output is 4.96 volts
+
+// Set up averaging for reading the ADC pins
+const int numSamples = 10; // Number of samples to average. Adjust as desired, noting more samples takes longer.
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
-  pinMode(LowFuelSwitch, INPUT_PULLUP);  
+  pinMode(LowFuelSwitch, INPUT);  
   pinMode(ledPin, OUTPUT);
   Serial.begin(9600);
 }
@@ -27,7 +30,9 @@ void setup() {
 void loop() {
   buttonState = digitalRead(buttonPin);
   switchState = digitalRead(LowFuelSwitch);
-
+  int rawA0;
+  int rawA1;
+  
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
       Serial.println("Button pressed");
@@ -42,6 +47,8 @@ void loop() {
     delay(50);
   }
   
+
+
   lastButtonState = buttonState;
 
   if (reading) {
@@ -50,34 +57,37 @@ void loop() {
     if (currentMillis - startMillis <= period) {
       int rawA0 = analogRead(A0);
       int rawA1 = analogRead(A1);
-      analog0 = rawA0 * 5.0 / 1024;
-      analog1 = rawA1 * 5.0 / 1024;
       
-      if (analog0 < UpperRheostatLowest) UpperRheostatLowest = analog0;
-      if (analog0 > UpperRheostatHighest) UpperRheostatHighest = analog0;
-      if (analog1 < MainRheostatLowest) MainRheostatLowest = analog1;
-      if (analog1 > MainRheostatHighest) MainRheostatHighest = analog1;      
+      if (rawA1 < UpperRheostatLowest) UpperRheostatLowest = rawA1;
+      if (rawA1 > UpperRheostatHighest) UpperRheostatHighest = rawA1;
+      
+      if (rawA0 < MainRheostatLowest) MainRheostatLowest = rawA0;
+      if (rawA0 > MainRheostatHighest) MainRheostatHighest = rawA0;      
     } else {
       Serial.print("Upper Rheostat Lowest: ");
-      Serial.println(UpperRheostatLowest * (5.0 / 1023.0));
+      Serial.println(UpperRheostatLowest * (ReferenceVoltage / 1023.0));
+      
       Serial.print("Upper Rheostat Highest: ");
-      Serial.println(UpperRheostatHighest * (5.0 / 1023.0));
+      Serial.println(UpperRheostatHighest * (ReferenceVoltage / 1023.0));
+      Serial.print("Upper Rheostat Raw: ");
+      Serial.println(analogRead(A1));
+            
       // Calculate and print the derived resistance of the Upper rheostat
-      float upperRheostatResistance = UpperRheostatHighest * (5.0 / 1023.0) / (5.0 - UpperRheostatHighest * (5.0 / 1023.0)) * UpperRheostatFixedResistor;
+      float upperRheostatResistance = UpperRheostatLowest * (ReferenceVoltage / 1023.0) / (ReferenceVoltage - UpperRheostatLowest * (ReferenceVoltage / 1023.0)) * UpperRheostatFixedResistor;
       Serial.print("Derived Upper Rheostat Resistance: ");
       Serial.println(upperRheostatResistance);
       
       Serial.print("Main Rheostat Lowest: ");
-      Serial.println(MainRheostatLowest * (5.0 / 1023.0));
+      Serial.println(MainRheostatLowest * (ReferenceVoltage / 1023.0));
       Serial.print("Main Rheostat Highest: ");
-      Serial.println(MainRheostatHighest * (5.0 / 1023.0));
+      Serial.println(MainRheostatHighest * (ReferenceVoltage / 1023.0));
       
-      float mainRheostatResistance = MainRheostatHighest * (5.0 / 1023.0) / (5.0 - MainRheostatHighest * (5.0 / 1023.0)) * MainRheostatFixedResistor;
+      float mainRheostatResistance = MainRheostatLowest * (ReferenceVoltage / 1023.0) / (ReferenceVoltage - MainRheostatLowest * (ReferenceVoltage / 1023.0)) * MainRheostatFixedResistor;
       Serial.print("Derived Main Rheostat Resistance: ");
       Serial.println(mainRheostatResistance);
       
       Serial.print("Low Fuel Switch State: ");
-      if (switchState == HIGH) {
+      if (switchState == LOW) {
         Serial.println("OPEN");
       } else {
         Serial.println("CLOSED");
