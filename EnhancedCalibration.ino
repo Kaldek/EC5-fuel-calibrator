e90a0426fa1a2688cd1fb8f60517266eff190fcf
Kaldek <ed.luck@gmail.com>
2024-01-08 22:19:30 +1100

    Fuel gauge needle position tuning
    Existing PWM values between F and 3/4 updated.

--- EnhancedCalibration.ino
+++ EnhancedCalibration.ino
@@ -24,68 +24,68 @@ const long interval = 5000; // 5 seconds interval
 // Both sources use ADC integers converted to voltage values.  This allows for alteration of the reference voltage without further changes to the code.
 FuelLevelRange primaryFuelLevelRanges[] = {
     {0, 3.18, 5.10},
-    {4, 3.15, 3.17},
-    {5, 3.11, 3.14},
-    {6, 3.08, 3.10},
-    {7, 3.06, 3.07},
-    {8, 3.04, 3.05},
-    {9, 2.99, 3.03},
-    {10, 2.96, 2.98},
-    {11, 2.91, 2.95},
-    {12, 2.88, 2.90},
-    {13, 2.82, 2.87},
-    {14, 2.77, 2.81},
-    {15, 2.71, 2.76},
-    {16, 2.65, 2.70},
-    {17, 2.53, 2.64},
-    {18, 2.48, 2.52},
-    {19, 2.42, 2.47},
-    {20, 2.34, 2.41},
-    {21, 2.22, 2.33},
-    {22, 2.05, 2.21},
-    {23, 1.99, 2.04},
-    {24, 1.84, 1.98},
-    {25, 1.73, 1.83},
-    {26, 1.66, 1.72},
-    {27, 1.61, 1.65},
-    {28, 1.56, 1.60},
-    {29, 1.45, 1.55},
-    {30, 1.42, 1.44},
-    {31, 1.18, 1.41},
-    {32, 0.00, 1.17}
+    {1, 3.15, 3.17},
+    {2, 3.11, 3.14},
+    {3, 3.08, 3.10},
+    {4, 3.06, 3.07},
+    {5, 3.04, 3.05},
+    {6, 2.99, 3.03},
+    {7, 2.96, 2.98},
+    {8, 2.91, 2.95},
+    {9, 2.88, 2.90},
+    {10, 2.82, 2.87},
+    {11, 2.77, 2.81},
+    {12, 2.71, 2.76},
+    {13, 2.65, 2.70},
+    {14, 2.53, 2.64},
+    {15, 2.48, 2.52},
+    {16, 2.42, 2.47},
+    {17, 2.34, 2.41},
+    {18, 2.22, 2.33},
+    {19, 2.05, 2.21},
+    {20, 1.99, 2.04},
+    {21, 1.84, 1.98},
+    {22, 1.73, 1.83},
+    {23, 1.66, 1.72},
+    {24, 1.61, 1.65},
+    {25, 1.56, 1.60},
+    {26, 1.45, 1.55},
+    {27, 1.42, 1.44},
+    {28, 1.18, 1.41},
+    {29, 0.00, 1.17}
 };
 
 FuelLevelRange secondaryFuelLevelRanges[] = {
     {0, 3.16, 5.10},
-    {4, 3.07, 3.15},
-    {5, 3.05, 3.06},
-    {6, 3.02, 3.04},
-    {7, 2.97, 3.01},
-    {8, 2.92, 2.98},
-    {9, 2.87, 2.91},
-    {10, 2.81, 2.86},
-    {11, 2.77, 2.80},
-    {12, 2.68, 2.76},
-    {13, 2.65, 2.67},
-    {14, 2.57, 2.64},
-    {15, 2.48, 2.56},
-    {16, 2.43, 2.47},
-    {17, 2.36, 2.42},
-    {18, 2.24, 2.35},
-    {19, 2.22, 2.23},
-    {20, 2.10, 2.21},
-    {21, 2.04, 2.09},
-    {22, 1.87, 2.03},
-    {23, 1.84, 1.86},
-    {24, 1.61, 1.83},
-    {25, 1.45, 1.60},
-    {26, 1.42, 1.44},
-    {27, 1.37, 1.41},
-    {28, 1.29, 1.36},
-    {29, 1.21, 1.30},
-    {30, 1.01, 1.20},
-    {31, 0.79, 1.00},
-    {32, 0.00, 0.78}
+    {1, 3.07, 3.15},
+    {2, 3.05, 3.06},
+    {3, 3.02, 3.04},
+    {4, 2.97, 3.01},
+    {5, 2.92, 2.98},
+    {6, 2.87, 2.91},
+    {7, 2.81, 2.86},
+    {8, 2.77, 2.80},
+    {9, 2.68, 2.76},
+    {10, 2.65, 2.67},
+    {11, 2.57, 2.64},
+    {12, 2.48, 2.56},
+    {13, 2.43, 2.47},
+    {14, 2.36, 2.42},
+    {15, 2.24, 2.35},
+    {16, 2.22, 2.23},
+    {17, 2.10, 2.21},
+    {18, 2.04, 2.09},
+    {19, 1.87, 2.03},
+    {20, 1.84, 1.86},
+    {21, 1.61, 1.83},
+    {22, 1.45, 1.60},
+    {23, 1.42, 1.44},
+    {24, 1.37, 1.41},
+    {25, 1.29, 1.36},
+    {26, 1.21, 1.30},
+    {27, 1.01, 1.20},
+    {28, 0.79, 1.00},
+    {29, 0.00, 0.78}
 };
 
 struct GaugeRange {
@@ -111,10 +111,10 @@ const GaugeRange GaugeRanges[] = {
   {37, 39, 199},
   {40, 42, 204},
   {43, 45, 208},  // Three Quarter tank
-  {46, 49, 227},
-  {50, 53, 238},
-  {54, 57, 249},
-  {58, 64, 250}   // FULL (58-64 litres)
+  {46, 49, 215},
+  {50, 53, 225},
+  {54, 57, 235},
+  {58, 64, 248}   // FULL (58-64 litres)
 };
 
 const int numPrimaryLevels = sizeof(primaryFuelLevelRanges) / sizeof(FuelLevelRange);
@@ -132,10 +132,10 @@ int getFuelLevelFromReading(float reading, FuelLevelRange ranges[], int numRange
 void setup() {
   pinMode(Light, OUTPUT);
   digitalWrite(Light, HIGH);
-  analogWrite(Gauge,186);
+  analogWrite(Gauge,210);
   delay(1000);
   // Enable below if debugging
-  // Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop() {
@@ -204,15 +204,20 @@ void loop() {
     }
   
   // Enable the below if debugging
-/*
+  /*
   Serial.print("\033[2J\033[H");
   Serial.print("The total fuel level is: ");
   Serial.println(fuelLevel);
-  Serial.print("Main Rheostat Fuel Level:");
+  Serial.print("Main Rheostat Fuel Level: ");
   Serial.println(primaryFuelLevel);
-  Serial.print("Secondary Rheostat Fuel Level:");
+  Serial.print("Main Rheostat voltage: ");
+  Serial.println(primaryReading);
+  Serial.print("Secondary Rheostat Fuel Level: ");
   Serial.println(secondaryFuelLevel);
+  Serial.print("Secondary Rheostat voltage:");
+  Serial.println(secondaryReading);
+  
   delay(1000);
-*/ 
+ */
   }  
 }
