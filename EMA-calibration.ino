8fb956b94658efa89055baa42a4c0b3ab69578bc
Kaldek <ed.luck@gmail.com>
2024-12-21 00:06:57 +1100

    Major update to calibration
    Re-calibrated sender readings using in-car installation.  Remove light flashing for low fuel and restricted low fuel light to when left-side low fuel switch is closed.

--- EMA-calibration.ino
+++ EMA-calibration.ino
@@ -6,6 +6,8 @@ const int Light = 6;
 const int PrimarySender = A1;
 const int SecondarySender = A0;
 const float ReferenceVoltage = 4.99; // Nano Every +5V output
+const int LowFuelSwitch = 3;
+int switchState = 0;
 
 int lightFlag;
 
@@ -27,69 +29,39 @@ bool emaInitialized = false; // Flag for initial EMA setup
 
 // Initialize an array of FuelLevelRange structures for Primary and Secondary sources.
 FuelLevelRange primaryFuelLevelRanges[] = {
-    {0, 3.18, 5.10},
-    {1, 3.15, 3.17},
-    {2, 3.11, 3.14},
-    {3, 3.08, 3.10},
-    {4, 3.06, 3.07},
-    {5, 3.04, 3.05},
-    {6, 2.99, 3.03},
-    {7, 2.96, 2.98},
-    {8, 2.91, 2.95},
-    {9, 2.88, 2.90},
-    {10, 2.82, 2.87},
-    {11, 2.77, 2.81},
-    {12, 2.71, 2.76},
-    {13, 2.65, 2.70},
-    {14, 2.53, 2.64},
-    {15, 2.48, 2.52},
-    {16, 2.42, 2.47},
-    {17, 2.34, 2.41},
-    {18, 2.22, 2.33},
-    {19, 2.05, 2.21},
-    {20, 1.99, 2.04},
-    {21, 1.84, 1.98},
-    {22, 1.73, 1.83},
-    {23, 1.66, 1.72},
-    {24, 1.61, 1.65},
-    {25, 1.56, 1.60},
-    {26, 1.45, 1.55},
-    {27, 1.42, 1.44},
-    {28, 1.18, 1.41},
-    {29, 0.00, 1.17}
+    {0, 3.33, 5.00},
+    {4, 3.29, 3.32},
+    {6, 3.22, 3.28},
+    {8, 3.13, 3.21},
+    {10, 3.06, 3.12},
+    {12, 2.99, 3.05},
+    {14, 2.81, 2.98},
+    {16, 2.69, 2.80},
+    {18, 2.57, 2.70},
+    {20, 2.40, 2.56},
+    {22, 2.20, 2.39},
+    {24, 2.02, 2.19},
+    {26, 1.57, 2.01},
+    {28, 1.20, 1.56},
+    {30, 0.74, 1.19},
+    {32, 0.39, 0.73}
 };
 
 FuelLevelRange secondaryFuelLevelRanges[] = {
-    {0, 3.16, 5.10},
-    {1, 3.07, 3.15},
-    {2, 3.05, 3.06},
-    {3, 3.02, 3.04},
-    {4, 2.97, 3.01},
-    {5, 2.92, 2.98},
-    {6, 2.87, 2.91},
-    {7, 2.81, 2.86},
-    {8, 2.77, 2.80},
-    {9, 2.68, 2.76},
-    {10, 2.65, 2.67},
-    {11, 2.57, 2.64},
-    {12, 2.48, 2.56},
-    {13, 2.43, 2.47},
-    {14, 2.36, 2.42},
-    {15, 2.24, 2.35},
-    {16, 2.22, 2.23},
-    {17, 2.10, 2.21},
-    {18, 2.04, 2.09},
-    {19, 1.87, 2.03},
-    {20, 1.84, 1.86},
-    {21, 1.61, 1.83},
-    {22, 1.45, 1.60},
-    {23, 1.42, 1.44},
-    {24, 1.37, 1.41},
-    {25, 1.29, 1.36},
-    {26, 1.21, 1.30},
-    {27, 1.01, 1.20},
-    {28, 0.79, 1.00},
-    {29, 0.00, 0.78}
+    {0, 3.25, 5.00},
+    {4, 3.21, 3.24},
+    {6, 3.12, 3.20},
+    {8, 2.98, 3.11},
+    {10, 2.86, 2.99},
+    {12, 2.72, 2.87},
+    {14, 2.57, 2.73},
+    {16, 2.38, 2.58},
+    {18, 2.19, 2.39},
+    {20, 1.88, 2.20},
+    {22, 1.68, 1.87},
+    {24, 1.40, 1.69},
+    {26, 1.20, 1.39},
+    {28, 0.44, 1.19}
 };
 
 struct GaugeRange {
@@ -100,25 +72,21 @@ struct GaugeRange {
 
 // Fuel Gauge needle position table
 const GaugeRange GaugeRanges[] = {
-  {0, 7, 141},  // EMPTY (0-7 litres in tank)
-  {8, 9, 151},
-  {10, 12, 160},
-  {13, 14, 166},
-  {15, 15, 171},  // Quarter tank
-  {16, 18, 174},
-  {19, 21, 176},
-  {22, 24, 177},
-  {25, 27, 181},
-  {28, 30, 183},  // Half tank
-  {31, 33, 190},
+  {0, 3, 141},  // EMPTY (0-3 litres in tank)
+  {4, 6, 151},
+  {8, 10, 160},
+  {12, 14, 166},
+  {16, 18, 171},  // Quarter tank
+  {20, 22, 174},
+  {24, 26, 176},
+  {28, 32, 183},  // Half tank
   {34, 36, 194},
-  {37, 39, 199},
-  {40, 42, 204},
-  {43, 45, 208},  // Three Quarter tank
-  {46, 49, 215},
-  {50, 53, 225},
-  {54, 57, 235},
-  {58, 64, 248}   // FULL (58-64 litres)
+  {38, 40, 199},
+  {42, 44, 204},
+  {46, 48, 208},  // Three Quarter tank
+  {50, 52, 215},
+  {54, 56, 225},
+  {58, 60, 248}   // FULL
 };
 
 const int numPrimaryLevels = sizeof(primaryFuelLevelRanges) / sizeof(FuelLevelRange);
@@ -134,17 +102,25 @@ int getFuelLevelFromReading(float reading, FuelLevelRange ranges[], int numRange
 }
 
 void setup() {
+  pinMode(LowFuelSwitch, INPUT);  
   pinMode(Light, OUTPUT);
   digitalWrite(Light, HIGH);
-  analogWrite(Gauge, 210);
-  delay(1000);
+  analogWrite(Gauge, 255);
+  unsigned long startTime = millis();
+  while (millis() - startTime < 10000) {
+  }
 }
 
 void loop() {
+  switchState = digitalRead(LowFuelSwitch);
+  analogRead(PrimarySender);
+  delayMicroseconds(20);
   int primaryRawReading = analogRead(PrimarySender);
+  delayMicroseconds(20);
   int secondaryRawReading = analogRead(SecondarySender);
   float primaryReading = primaryRawReading * (ReferenceVoltage / 1023.0);
   float secondaryReading = secondaryRawReading * (ReferenceVoltage / 1023.0);
+  
 
   // Round the results to two decimal places
   primaryReading = round(primaryReading * 100) / 100.0;
@@ -152,10 +128,10 @@ void loop() {
 
   int primaryFuelLevel = getFuelLevelFromReading(primaryReading, primaryFuelLevelRanges, numPrimaryLevels);
   int secondaryFuelLevel = getFuelLevelFromReading(secondaryReading, secondaryFuelLevelRanges, numSecondaryLevels);
-
+  
   // Get the combined fuel level
   int currentFuelLevel = primaryFuelLevel + secondaryFuelLevel;
-
+  
   // EMA calculation
   if (!emaInitialized) {
     emaFuelLevel = currentFuelLevel; // Initialize EMA with the first reading
@@ -171,30 +147,21 @@ void loop() {
 
   // Update light flag every interval
   if ((fuelLevel != lastFuelLevel) && (currentTime - lastUpdateTime >= interval)) {
-    if (fuelLevel <= 6) {
-      lightFlag = 3; // Fast flash
-    } else if (fuelLevel <= 8) {
-      lightFlag = 2; // Slow flash
-    } else if (fuelLevel <= 11) {
+    if ((fuelLevel <= 11) && (switchState == LOW)) {
       lightFlag = 1; // Steady on
-    } else {
-      lightFlag = 0; // Off
+    
     }
 
     lastFuelLevel = fuelLevel;
     lastUpdateTime = currentTime;
   }
 
+   
   // Handle the light based on the lightFlag
   switch (lightFlag) {
     case 1:
       digitalWrite(Light, HIGH); break;
-    case 2:
-      digitalWrite(Light, HIGH); delay(600);
-      digitalWrite(Light, LOW); delay(600); break;
-    case 3:
-      digitalWrite(Light, HIGH); delay(200);
-      digitalWrite(Light, LOW); delay(200); break;
+    
     default:
       digitalWrite(Light, LOW); break;
   }
@@ -203,6 +170,7 @@ void loop() {
   for (int i = 0; i < sizeof(GaugeRanges) / sizeof(GaugeRanges[0]); ++i) {
     if (fuelLevel >= GaugeRanges[i].LowestLitre && fuelLevel <= GaugeRanges[i].HighestLitre) {
       analogWrite(Gauge, GaugeRanges[i].pwm);
+      break;
     }
   }
 }
